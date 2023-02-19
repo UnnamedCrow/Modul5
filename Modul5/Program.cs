23d14096444ef6b57e36223efb098dcea1dc844d
Максим Самисько <masechk@gmail.com>
2023-02-19 12:48:15 +0300

    Modul5 - FinalTask: Added output function for tuple with users information

--- Modul5/Program.cs
+++ Modul5/Program.cs
@@ -10,9 +10,9 @@ namespace Modul5
 {
     internal class Program
     {
-        static (string Firstname, string Surname, byte Age, bool HavePet, byte PetCount, string[] PetName, string[] FavouriteColors) InputData()
+        static (string Firstname, string Surname, byte Age, bool HavePet, string[] PetName, string[] FavouriteColors) InputData()
         {
-            (string Firstname, string Surname, byte Age, bool HavePet, byte PetCount, string[] PetName, string[] FavouriteColors) result;
+            (string Firstname, string Surname, byte Age, bool HavePet, string[] PetName, string[] FavouriteColors) result;
 
             // Enter FirstName
             Console.Write("Enter your firstname: ");
@@ -22,35 +22,73 @@ namespace Modul5
             Console.Write("Enter your surtname: ");
             result.Surname = Console.ReadLine();
 
-            // Enter Age and chech input
-            Console.Write("Enter your age: ");
-            while (!byte.TryParse(Console.ReadLine(),out result.Age))
-            {
-                Console.Write("Oooops! Please try again. Enter your age:");
-            }
+            // Enter Age and validate intup data
+            result.Age = GetUserAge();
 
             // Enter pets information
             result.HavePet = HavePetFunc();
             if (result.HavePet == true)
-            {
-                result.PetCount = PetCountFunc();
-                result.PetName = PetNameFunc(result.PetCount);
-            }
+                result.PetName = GetPetName(PetCountFunc());
+            else
+                result.PetName = null;
 
-
+            // Enter colors information
+            if (GetNumColors() > 0)
+                result.FavouriteColors = GetFavColors(GetNumColors());
+            else
+                result.FavouriteColors = null;
             return result;
         }
-        /// <summary>
+
+        /// Function for get users age
+        /// and validate input data
+        static byte GetUserAge()
+        {
+            Console.Write("Enter your age: ");
+            byte result = 0;
+            if(byte.TryParse(Console.ReadLine(), out result) && result > 0)
+                return result;
+            else
+            {
+                Console.Write("Oooops! Please try again. Enter your age:");
+                return GetUserAge();
+            }
+        }
+
+        /// Get num of favourite colors 
+        /// and validate data
+        /// <returns>num of favourite colors</returns>
+        static int GetNumColors()
+        {
+            int result = 0;
+            Console.Write("Enter num of your favourite colors: ");
+            if(int.TryParse(Console.ReadLine(), out result) && result > 0)
+                return result;
+            else
+                return 0;
+        }
+
+        /// Get favourite colors from user
+        static string[] GetFavColors(int ColorsCount)
+        {
+            string[] result = new string[ColorsCount];
+            for (int i = 0; i < ColorsCount; i++)
+            {
+                Console.Write("Please enter your favourite color number {0}: ", i + 1);
+                result[i] = Console.ReadLine();
+            }
+            return result;
+        }
+
         /// enter pets names
-        /// </summary>
         /// <param name="PetCount"></param>
         /// <returns>pet names</returns>
-        static string[] PetNameFunc (int PetCount)
+        static string[] GetPetName(int PetCount)
         {
             string[] result = new string[PetCount];
-            for(int i = 0; i < PetCount; i++)
+            for (int i = 0; i < PetCount; i++)
             {
-                Console.Write("Please enter name your pet number {0}", i + 1);
+                Console.Write("Please enter name your pet number {0}: ", i + 1);
                 result[i] = Console.ReadLine();
             }
             return result;
@@ -58,13 +96,14 @@ namespace Modul5
 
         /// <summary>
         /// User enters information about his pets
+        /// validation 
         /// </summary>
         /// <returns>bool</returns>
         static byte PetCountFunc()
         {
             byte result = 0;
             Console.Write("How many pets do you have: ");
-            if(byte.TryParse(Console.ReadLine(), out result))
+            if (byte.TryParse(Console.ReadLine(), out result) && result > 0)
                 return result;
             else
             {
@@ -75,6 +114,7 @@ namespace Modul5
 
         /// <summary>
         /// User enters information about his pets
+        /// validation
         /// </summary>
         /// <returns>bool</returns>
         static bool HavePetFunc()
@@ -92,9 +132,37 @@ namespace Modul5
                 return HavePetFunc();
             }
         }
+
+        /// <summary>
+        /// output function
+        /// for user tuple
+        /// </summary>
+        static void OutputData((string Firstname, string Surname, byte Age, bool HavePet, string[] PetName, string[] FavouriteColors) User)
+        {
+            /// Output Users firstname and surname
+            Console.WriteLine("Hello dear, {0} {1}", User.Firstname, User.Surname);
+
+            /// Output Users Age
+            Console.WriteLine("Your age is {0} years", User.Age);
+
+            /// Output users pets names if user have pets
+            if (User.HavePet)
+
+                for(int i = 0; i < User.PetName.Length; i++)
+
+                    Console.WriteLine($"Name of your pet number {i + 1} is {User.PetName[i]}");
+
+            /// Output users favourite colors if user have favourite colors
+            if (User.FavouriteColors != null)
+
+                for (int i = 0; i < User.FavouriteColors.Length; i++)
+                    Console.WriteLine($"Your favourite color number {i + 1} is {User.FavouriteColors[i]}");
+
+
+        }
         static void Main()
         {
-             
+            var User = InputData();
             Console.ReadLine();
         }
     }
